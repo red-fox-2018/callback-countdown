46f02ad7bd82cb691fb115ff829c170c5583d88a
ihsanmaulanahamdani <ihsanmaulana5@gmail.com>
2018-04-12 15:49:50 +0700

    done

--- controllers.js
+++ controllers.js
@@ -2,6 +2,13 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
+  let interval = setInterval(() => {
+    displayCountdown(seconds);
+    seconds--;
+    if (seconds < 0) {
+      clearInterval(interval);
+    }
+  }, 1000);
 };
 
 module.exports = {
