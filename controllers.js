b924ed112cb320ffb16f9f2be656c126e3de0d61
Iswanul Umam <umam@sepulsa.com>
2018-04-12 16:18:49 +0700

    finish callback countdown

--- controllers.js
+++ controllers.js
@@ -5,13 +5,16 @@ const figlet = require('figlet');
 const timer = seconds => {
   let intervalId = setInterval(function() {
     if (seconds >= 0) {
-      displayCountdown(seconds);
+      displayCountdown(konversiMenit(seconds));
     } else {
       clearInterval(intervalId);
     }
     seconds--;
   }, 1000);
 };
+
+const konversiMenit = (menit) => ((Math.floor(menit / 60) > 9 ? Math.floor(menit / 60) : `0${Math.floor(menit / 60)}` ) + ':' + ((menit % 60) < 10 ? ('0' + (menit % 60)) : (menit % 60)));
+
 
 module.exports = {
   timer
--- views.js
+++ views.js
@@ -6,7 +6,6 @@ const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
   console.log(chalk.cyan(figlet.textSync(seconds)));
-  // Your code here...
 };
 
 module.exports = { displayCountdown };
