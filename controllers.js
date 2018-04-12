d2d81201c7c886728091cf473bf3065cbc4a4999
restuu <restu_u@outlook.com>
2018-04-12 16:23:18 +0700

    first commit

--- controllers.js
+++ controllers.js
@@ -1,7 +1,21 @@
 const { displayCountdown } = require("./views");
 
 const timer = seconds => {
-  // Your code here...
+  let interval = setInterval(function () {
+    if (seconds >= 0 ) {
+      let min = Math.floor(seconds/60);
+      let sec = seconds % 60
+      if (String(sec).length === 1) {
+        sec = '0' + sec.toString()
+      }
+      let strTime = min + ' : ' + sec
+
+      displayCountdown(strTime)
+    } else {
+      clearInterval(interval)
+    }
+    seconds--
+  }, 1000);
 };
 
 module.exports = {
