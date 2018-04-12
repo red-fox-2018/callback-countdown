49ea5b6fe9781a88fbe1953ed7e53fa7dfe7b487
Renal Apriansyah <renal.apr@gmail.com>
2018-04-12 04:20:32 -0400

    add countdown

--- controllers.js
+++ controllers.js
@@ -2,8 +2,23 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
-};
-
+  setInterval(function () {
+    if(seconds>=0){
+      let menit_puluh = Math.floor(Math.floor(seconds / 60) / 10)
+      let menit_satuan = (Math.floor(seconds / 60) % 10)
+      let puluh = Math.floor((seconds % 60) / 10)
+      let satuan = (seconds % 60) % 10
+      let waktu = `${menit_puluh} ${menit_satuan} : ${puluh} ${satuan}`
+      displayCountdown(waktu)
+    }
+    else{
+      clearInterval(seconds)      
+    }
+    seconds -= 1
+  }, 1000);
+      
+}
+console.log(timer(10))
 module.exports = {
   timer
 };
