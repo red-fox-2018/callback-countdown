ea1c5672e3946db082b16f59a39bab8e0d7b09df
anugrah94 <anugrahrezkifauzinasution@gmail.com>
2018-04-12 17:56:14 +0700

    selesai anchor callback countdown

--- controllers.js
+++ controllers.js
@@ -1,7 +1,29 @@
 const { displayCountdown } = require("./views");
 
 const timer = seconds => {
-  // Your code here...
+  let number = 10;
+  let interval = setInterval(function(){ 
+    if(number !== 0){
+      let min = Math.floor(number/60)
+      let sec = number%60
+      if(min < 10 && sec <10){
+        displayCountdown(`0${min}:0${sec}`)
+      }
+      else if(min < 10 && sec > 10){
+        displayCountdown(`0${min}:${sec}`)
+      }
+      else if(min > 10 && sec > 10){
+        displayCountdown(`${min}:${sec}`)
+      }
+      
+      number--
+    }
+    else{
+      displayCountdown(`0${number}:0${number}`)
+      clearInterval(interval)
+    }
+   }, 1000);
+   
 };
 
 module.exports = {
