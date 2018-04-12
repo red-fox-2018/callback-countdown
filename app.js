a1512229d464953aa7570501cab52b3d0962a3cf
asepridwan1989 <aseprmail@gmail.com>
2018-04-12 17:19:05 +0700

    done

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const { timer } = require("./controllers");
 const main = () => {
   const seconds = parseInt(process.argv[2]) || 10;
   timer(seconds);
+  // console.log(seconds);
+  
 };
 
 main();
