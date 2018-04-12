66e30fafbd4fd5390d76a127af6672911e2fa813
yasirmaulana <yasir.maulana@gmail.com>
2018-04-12 17:19:45 +0700

    callback countdown

--- controllers.js
+++ controllers.js
@@ -1,9 +1,20 @@
 const { displayCountdown } = require("./views");
 
 const timer = seconds => {
-  // Your code here...
-};
+
+  var intervalID = setInterval(function() {
+    displayCountdown(seconds)
+    // console.log(seconds)
+    seconds--
+    if(seconds<0){
+      clearInterval(intervalID)
+    }
+ }, 1000);
+}
+
 
 module.exports = {
   timer
 };
+
+// console.log(myTimer());
