044365e98cc9e0f7e05273967f5719f240faf7a6
Faip Mardoni <faipmardoni@gmail.com>
2018-04-12 16:22:01 +0700

    The challenge has been completed

--- controllers.js
+++ controllers.js
@@ -2,7 +2,14 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
-};
+  var waktu = setInterval(function () {
+    displayCountdown(seconds);
+    seconds--
+    if(seconds < 0) {
+      clearInterval(waktu)
+    }
+  }, 1000);
+}
 
 module.exports = {
   timer
--- views.js
+++ views.js
@@ -3,9 +3,24 @@ const figlet = require("figlet");
 const chalk = require("chalk");
 
 const displayCountdown = seconds => {
+  let menit = Math.floor(seconds / 60);
+  let detik = seconds - (menit*60);
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
   // Your code here...
+  if (menit < 10) {
+    if(detik < 10) {
+      console.log(chalk.cyan(figlet.textSync(`0${menit}:0${detik}`)));
+    }else {
+      console.log(chalk.cyan(figlet.textSync(`0${menit}:${detik}`)));
+    }
+  }else {
+    if(detik < 10) {
+      console.log(chalk.cyan(figlet.textSync(`${menit}:0${detik}`)));
+    }else {
+      console.log(chalk.cyan(figlet.textSync(`${menit}:${detik}`)));
+    }
+  }
 };
 
 module.exports = { displayCountdown };
