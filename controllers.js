2b333f707a863ce9ce5a263b9674f4d3298a355c
violerine <viosefferine@gmail.com>
2018-04-12 21:13:27 +0700

    countdown

--- controllers.js
+++ controllers.js
@@ -1,8 +1,33 @@
 const { displayCountdown } = require("./views");
 
 const timer = seconds => {
+ 
   // Your code here...
+  var countdown = setInterval(function() {
+    seconds=seconds-1
+    var mins=Math.floor(seconds/60)>1? "0"+Math.floor(seconds/60):"00"
+    var sec= seconds%60==0?mins="01":seconds%60
+
+    if(sec>0){
+      sec-1
+    }
+    else{
+      if(mins>=1){
+        mins=mins-1
+        sec-1
+      }
+    }
+
+    console.log(mins+" : " +sec)
+
+    if (seconds <= 0) {
+        clearInterval(countdown);
+    }
+
+}, 1000);
 };
+
+
 
 module.exports = {
   timer
--- views.js
+++ views.js
@@ -1,11 +1,13 @@
 const clear = require("clear");
 const figlet = require("figlet");
 const chalk = require("chalk");
+const app = require("./app.js")
 
 const displayCountdown = seconds => {
   clear();
+  
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
-  // Your code here...
+  // Your code here... 
 };
 
 module.exports = { displayCountdown };
