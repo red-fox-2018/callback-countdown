a081e89e3fd5acc0e54131c79d8d76b56ad1f284
cantdocpp <cihuyman95@gmail.com>
2018-04-12 19:42:42 +0700

    first commit

--- controllers.js
+++ controllers.js
@@ -1,8 +1,27 @@
 const { displayCountdown } = require("./views");
 
 const timer = seconds => {
-  // Your code here...
-};
+  var interval = setInterval(function () {
+    var minute = 1;
+    while (seconds >= 60) {
+      minute += 1;
+      seconds -= 60;
+    }
+
+    if (seconds < 10) {
+      displayCountdown('00:0'+ seconds)
+    } else if (seconds < 60) {
+      displayCountdown('00:'+ seconds)
+    } else if (seconds > 60) {
+      displayCountdown('0' + minute + ':'+ seconds);
+    }
+
+    seconds--
+    if (seconds == -1) {
+      clearInterval(interval)
+    }
+  }, 1000)
+}
 
 module.exports = {
   timer
