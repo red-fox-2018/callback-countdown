0c845ba08cac0f6e90d9dcb6783eaf5138d3166d
Taufik Hidayat <ovick_kd@yahoo.com>
2018-04-12 18:22:46 +0700

    callback countdown

--- app.js
+++ app.js
@@ -2,7 +2,43 @@ const { timer } = require("./controllers");
 
 const main = () => {
   const seconds = parseInt(process.argv[2]) || 10;
-  timer(seconds);
+
+  let detik = seconds % 60 + 1
+  let menit = Math.floor(seconds / 60)
+  let interval = setInterval(() => {
+
+    if (menit > 0 && detik == 0) {
+      menit -= 1
+      detik = 60
+    }
+
+    if (detik == 0 && menit == 0) {
+
+      clearInterval(interval)
+
+    } else {
+
+      detik--
+
+      if (detik < 10) {
+        if (menit < 10) {
+          timer(`0${menit}:0${detik}`)
+        } else {
+          timer(`${menit}:0${detik}`)
+        }
+      } else {
+        if (menit < 10) {
+          timer(`0${menit}:${detik}`)
+        } else {
+          timer(`${menit}:${detik}`)
+        }
+      }
+
+    }
+
+  }, 800);
+
+
 };
 
 main();
--- controllers.js
+++ controllers.js
@@ -2,6 +2,8 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
+  displayCountdown(seconds)
+
 };
 
 module.exports = {
