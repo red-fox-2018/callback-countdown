ee48b9805b0f2d7f8ee09cbc43114672ee6f0f0b
asepridwan1989 <aseprmail@gmail.com>
2018-04-18 11:42:36 +0700

    revisi controllers, kemarin ke push yang kosong

--- controllers.js
+++ controllers.js
@@ -2,6 +2,12 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
+    let interval = setInterval(function(){
+    displayCountdown(seconds);
+    seconds--;
+    if (seconds < 0) {
+    clearInterval(interval);
+    }}, 1000);
 };
 
 module.exports = {
